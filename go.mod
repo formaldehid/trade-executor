215b613c691fc4145adc2f338536d80ee38a716a
Marko Sabec <marko@sabec.si>
2022-07-28 15:13:50 +0200

    add test for fill order

--- go.mod
+++ go.mod
@@ -6,15 +6,20 @@ require (
 	github.com/adshao/go-binance/v2 v2.3.8 // indirect
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-sqlite3 v1.14.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/urfave/cli/v2 v2.11.1 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/trader/book_test.go
@@ -0,0 +1,76 @@
+package trader
+
+import (
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type bookTestSuite struct {
+	suite.Suite
+}
+
+func TestBook(t *testing.T) {
+	suite.Run(t, new(bookTestSuite))
+}
+
+func (s *bookTestSuite) TestFillOrder() {
+
+	order := &Order{
+		Id:     uuid.NewString(),
+		Type:   "SELL",
+		Symbol: "BNBUSDT",
+		Size:   25,
+		Price:  42,
+	}
+
+	ticker1 := &Ticker{
+		Id:           400900217,
+		Symbol:       "BNBUSDT",
+		BestBidPrice: 40,
+		BestBidQty:   10,
+		BestAskPrice: 41,
+		BestAskQty:   10,
+	}
+
+	ticker2 := &Ticker{
+		Id:           400900223,
+		Symbol:       "BNBUSDT",
+		BestBidPrice: 42,
+		BestBidQty:   5,
+		BestAskPrice: 43,
+		BestAskQty:   10,
+	}
+
+	ticker3 := &Ticker{
+		Id:           400900235,
+		Symbol:       "BNBUSDT",
+		BestBidPrice: 42.5,
+		BestBidQty:   30,
+		BestAskPrice: 43,
+		BestAskQty:   10,
+	}
+
+	var orderFills []*OrderFill
+
+	cfg := NewConfig()
+	cfg.Symbol = "BNBUSDT"
+
+	book, _ := NewBook(cfg)
+
+	res, _ := book.FillOrder(order, ticker1, orderFills)
+	assert.Equal(s.T(), res, false)
+
+	res, ofs2 := book.FillOrder(order, ticker2, orderFills)
+	assert.Equal(s.T(), res, false)
+	assert.Equal(s.T(), ofs2.Price, ticker2.BestBidPrice)
+	assert.Equal(s.T(), ofs2.Size, ticker2.BestBidQty)
+	assert.Equal(s.T(), ofs2.Ticker.Id, ticker2.Id)
+
+	res, ofs3 := book.FillOrder(order, ticker3, append(orderFills, ofs2))
+	assert.Equal(s.T(), res, true)
+	assert.Equal(s.T(), ofs3.Price, 42.5)
+	assert.Equal(s.T(), ofs3.Size, 20.0)
+	assert.Equal(s.T(), ofs3.Ticker.Id, ticker3.Id)
+}
